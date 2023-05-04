5e6a24b2b148bee3c53cb97bf27fa451ac286175
LIDIANE DINIZ <lidianefernandesdiniz.lfd@gmail.com>
2023-05-03 21:59:47 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -4,6 +4,7 @@ export const api = axios.create({
    
  //baseURL: "http://localhost:3333"
    
-  baseURL: "https://api-food-explorer.herokuapp.com"
+ // baseURL: "https://api-food-explorer.herokuapp.com"
+   baseURL: process.env.API_URL
 
 });
